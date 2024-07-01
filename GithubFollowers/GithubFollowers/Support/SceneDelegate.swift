dbb88e3c3f71038a8d3a275199f6b8b997ca20c3
Rami Mustafa <ramimustafa77@gmail.com>
2024-07-01 23:01:58 +0300

    App navigation setup - UITabBarController and UINavigationController.

--- GithubFollowers/GithubFollowers/Support/SceneDelegate.swift
+++ GithubFollowers/GithubFollowers/Support/SceneDelegate.swift
@@ -13,14 +13,40 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let scene = (scene as? UIWindowScene) else { return }
-        let  window = UIWindow(windowScene: scene)
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+     
         
-        window.rootViewController = ViewController()
+        
+        let  window = UIWindow(windowScene: windowScene)
+        window.rootViewController = ccreateTabBar()
         self.window = window
         self.window?.makeKeyAndVisible()
+
     }
 
-   
+    func createSearchNC() -> UINavigationController {
+        let searchVC = SearchVC()
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search ,  tag: 0)
+        return UINavigationController(rootViewController: searchVC)
+        
+    }
+    
+    func createFavoritesNC() -> UINavigationController {
+        let FavoritesListVC = FavoritesListVC()
+        FavoritesListVC.title = "Favorite"
+        FavoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search ,  tag: 1)
+        return UINavigationController(rootViewController: FavoritesListVC)
+        
+    }
+    
+    
+    func ccreateTabBar() -> UITabBarController {
+        let tabbar = UITabBarController()
+        UITabBar.appearance().tintColor = .systemGreen
+        tabbar.viewControllers = [createSearchNC() , createFavoritesNC()]
+     
+        return tabbar
+    }
 }
 
