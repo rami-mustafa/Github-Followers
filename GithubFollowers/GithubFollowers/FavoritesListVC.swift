48d3e6fceb400bb41c796929bb0ab7969e0d0458
Rami Mustafa <ramimustafa77@gmail.com>
2024-07-01 23:00:42 +0300

    Create a FavoritesListVC page.

---
+++ GithubFollowers/GithubFollowers/FavoritesListVC.swift
@@ -0,0 +1,21 @@
+//
+//  FavoritesListVC.swift
+//  GithubFollowers
+//
+//  Created by Rami Mustafa on 01.07.24.
+//
+
+import UIKit
+
+class FavoritesListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemGray
+    }
+    
+
+ 
+
+}
