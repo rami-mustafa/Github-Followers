32ba4fe879f57fc3f1b9e15f34358aa4d4d9276a
Rami Mustafa <ramimustafa77@gmail.com>
2024-07-02 15:50:38 +0300

    Create a custom Button and TextField.

---
+++ GithubFollowers/GithubFollowers/GFButton.swift
@@ -0,0 +1,40 @@
+//
+//  GFButton.swift
+//  GithubFollowers
+//
+//  Created by Rami Mustafa on 02.07.24.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    convenience init(color: UIColor, title: String, systemImageName: String){
+        self.init(frame: .zero)
+        set(color: color, title: title, systemImageName: systemImageName)
+    }
+    
+    private func configure(){
+//      tinted tarzında olmasını sağlar. Bu, butonun hem arka plan rengini hem de metin ve simge rengini aynı renk yapar.
+        configuration = .tinted()
+        configuration?.cornerStyle = .medium
+    }
+    
+    func set(color: UIColor, title: String, systemImageName: String){
+        configuration?.baseBackgroundColor = color
+        configuration?.baseForegroundColor = color
+        configuration?.title = title
+        configuration?.image = UIImage(systemName: systemImageName)
+        configuration?.imagePadding = 6
+        configuration?.imagePlacement = .trailing
+    }
+}
