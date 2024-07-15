acdc3108569a9cee5d500a28c080411f0b2e2300
Rami Mustafa <ramimustafa77@gmail.com>
2024-07-15 23:40:32 +0300

    Update folder and created new pages

---
+++ GithubFollowers/GithubFollowers/Screens/UserInfoVC.swift
@@ -0,0 +1,28 @@
+//
+//  UserInfoVC.swift
+//  GithubFollowers
+//
+//  Created by Rami Mustafa on 15.07.24.
+//
+
+import UIKit
+
+class UserInfoVC: UIViewController {
+
+    // MARK: - Variables
+    
+    // MARK: - UI Components
+    
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupView()
+    }
+    // MARK: - UI Setup
+    private func setupView(){}
+    
+    // MARK: - Selectors
+
+
+}
