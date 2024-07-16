bd3bbdebb348406ab0aebb3704b48ebf0777fd84
Rami Mustafa <ramimustafa77@gmail.com>
2024-07-16 23:41:24 +0300

    Create a Network Manager

---
+++ GithubFollowers/GithubFollowers/NetworkManager.swift
@@ -0,0 +1,71 @@
+//
+//  NetworkManager.swift
+//  GithubFollowers
+//
+//  Created by Rami Mustafa on 16.07.24.
+//
+
+import Foundation
+
+
+import UIKit
+
+class NetworkManager{
+    static let shared = NetworkManager()
+    private let baseUrl = "https://api.github.com/users/"
+    let cache = NSCache<NSString, UIImage>()
+    let decoder = JSONDecoder()
+    private init () {
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = .iso8601
+    }
+    
+   
+    
+    func getFollowers(for username: String, page: Int) async throws -> [Follower]{
+        let endpoint = baseUrl + "\(username)/followers?per_page=100&page=\(page)"
+        guard let url = URL(string: endpoint) else {
+            throw GFError.invalidUsername
+        }
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+            throw GFError.invalidResponse
+        }
+        do{
+            return try decoder.decode([Follower].self, from: data)
+        } catch{
+            throw GFError.invalidData
+        }
+    }
+    
+    
+    func getUserInfo(for username: String) async throws -> User{
+        let endpoint = baseUrl + "\(username)"
+        guard let url = URL(string: endpoint) else {
+            throw GFError.invalidUsername
+        }
+        let (data, response) = try await URLSession.shared.data(from: url)
+        guard let response = response as? HTTPURLResponse, response.statusCode == 200 else{
+            throw GFError.invalidResponse
+        }
+        do{
+            return try decoder.decode(User.self, from: data)
+        } catch{
+            throw GFError.invalidData
+        }
+    }
+    
+    func downloadImage(from urlString: String) async -> UIImage?{
+        let cacheKey = NSString(string: urlString)
+        if let image = cache.object(forKey: cacheKey){ return image }
+        guard let url = URL(string: urlString) else { return nil }
+        do{
+            let (data, _) = try await URLSession.shared.data(from: url)
+            guard let image = UIImage(data: data) else { return nil }
+            cache.setObject(image, forKey: cacheKey)
+            return image
+        } catch{
+            return nil
+        }
+    }
+}
